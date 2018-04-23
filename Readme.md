b2e497f85d997cb1ff71442ec2ea7d6433119dd4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# How to animate gauge elements
+
+
+<p>This example demonstrates how to use animation effects in DXGauges. </p>
+
+
+<h3>Description</h3>
+
+<p>To activate animation, set the  <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfGaugesGaugeControlBase_EnableAnimationtopic"><u>GaugeControlBase.EnableAnimation</u></a> property to <strong>t</strong><strong>rue</strong>. After that it becomes possible to customize animation&#39;s duration and easing function via its <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfGaugesIndicatorAnimation_Durationtopic"><u>IndicatorAnimation.Duration</u></a>  and <a href="http://documentation.devexpress.com/#Silverlight/DevExpressXpfGaugesIndicatorAnimation_EasingFunctiontopic"><u>IndicatorAnimation.EasingFunction</u></a> properties.</p><br />
+
+
+<br/>
+
+
---
+++ VB/GaugesAnimation/My Project/AssemblyInfo.vb
@@ -0,0 +1,36 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("GaugesAnimation")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("GaugesAnimation")>
+<Assembly: AssemblyCopyright("Copyright ©  2013")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("64981ad8-6ef7-46f5-b135-42bb80a2a710")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Revision and Build Numbers 
+' by using the '*' as shown below:
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
